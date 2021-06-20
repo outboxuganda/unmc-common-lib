8d8e7bb1263d4f166d5e6e2fdffabe18da543b2d
codephillip <codephillip@gmail.com>
2021-06-20 13:15:36 +0300

    ignored build and node_module folders

--- build/events/subjects.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Subjects;
-(function (Subjects) {
-    Subjects["TicketCreated"] = "ticket:created";
-    Subjects["TicketUpdated"] = "ticket:updated";
-})(Subjects = exports.Subjects || (exports.Subjects = {}));
