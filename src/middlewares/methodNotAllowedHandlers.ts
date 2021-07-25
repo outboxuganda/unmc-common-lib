86a642ace44d4e400ff3ea746a7550f5eb638ab8
codephillip <codephillip@gmail.com>
2021-07-25 13:55:02 +0300

    Updates

--- src/middlewares/methodNotAllowedHandlers.ts
+++ src/middlewares/methodNotAllowedHandlers.ts
@@ -1,4 +1,4 @@
-import {MethodNotAllowed} from "../utils/errors";
+import MethodNotAllowed from "../utils/errors/MethodNotAllowed";
 
 export const methodNotAllowedHandlers = () => {
     throw new MethodNotAllowed('Method not allowed');
