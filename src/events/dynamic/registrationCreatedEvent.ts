0f8c0c8db59a384fd88a1441d5da7643fbc4ddbb
codephillip <codephillip@gmail.com>
2021-10-13 06:55:04 +0300

    Updates

--- src/events/dynamic/registrationCreatedEvent.ts
+++ src/events/dynamic/registrationCreatedEvent.ts
@@ -5,6 +5,7 @@ export interface RegistrationCreatedEvent {
     data: {
         userId: string;
         registrationId: string;
+        serviceType: string;
         status: 'completed' | 'pending' | 'delayed' | 'cancelled';
         category: 'permanent' | 'temporary_bachelor' | 'temporary_extensor' |
             'temporary_foreign_trained' | 'register_only' | 'private' | 'renewal';
