2bd938526bbcc9bdc60e291c967d6102199f5740
codephillip <codephillip@gmail.com>
2022-03-04 09:24:21 +0300

    Updates

--- src/events/dynamic/documentReviewedEvent.ts
+++ src/events/dynamic/documentReviewedEvent.ts
@@ -2,5 +2,15 @@ import {Subjects} from './subjects';
 
 export interface DocumentReviewedEvent {
     subject: Subjects.DocumentReviewed;
-    data: object;
+    data: {
+        id: string,
+        name: string,
+        createdAt: Date
+        url: string,
+        fileType: string,
+        valid: boolean,
+        status: string,
+        verificationReason: string,
+        userId: string
+    }
 }
