6d22b9352cfb11090f86fbd6dc7123842cb5ebc5
codephillip <codephillip@gmail.com>
2022-04-13 00:01:42 +0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,7 @@ export * from './events/dynamic/userLogInAttemptEvent';
 export * from './events/dynamic/interviewActiveEvent';
 export * from './events/dynamic/userRegisteredEvent';
 export * from './events/dynamic/registrationReviewedEvent';
+export * from './events/dynamic/inspectionSubmittedEvent';
 export * from './events/dynamic/userInfoUpdatedEvent';
 export * from './events/dynamic/documentReviewedEvent';
 export * from './events/dynamic/queueGroupNames';
