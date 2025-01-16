ab160d3bf424c9b5a11149cda4973fce9fa86aa4
edna margaret <edna.margaret@outbox.co.ug>
2025-01-16 10:21:52 +0300

    Updates

--- src/events/core/base-listener.ts
+++ src/events/core/base-listener.ts
@@ -17,10 +17,12 @@ export abstract class Listener<T extends Event> {
     protected ackWait = this.seconds * 1000;
 
     constructor(client: Stan) {
+        console.log(`Listener initialized with client: ${client}`);
         this.client = client;
     }
 
     subscriptionOptions() {
+        console.log(`Setting subscription options for queue group: ${this.queueGroupName}`);
         return this.client
             .subscriptionOptions()
             .setDeliverAllAvailable()
@@ -30,6 +32,7 @@ export abstract class Listener<T extends Event> {
     }
 
     listen() {
+        console.log(`Preparing to subscribe to subject: ${this.subject} in queue group: ${this.queueGroupName}`);
         const subscription = this.client.subscribe(
             this.subject,
             this.queueGroupName,
@@ -42,12 +45,20 @@ export abstract class Listener<T extends Event> {
             console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
 
             const parsedData = this.parseMessage(msg);
+            console.log(`Processing message for subject: ${this.subject}`);
             this.onMessage(parsedData, msg);
+            console.log(`Processed message for subject: ${this.subject}`);
+            
         });
     }
 
     parseMessage(msg: Message) {
         const data = msg.getData();
+        console.log(`Raw message data: ${data}`);
+        console.log(typeof data === 'string'
+            ? JSON.parse(data)
+            : JSON.parse(data.toString('utf8')))
+            
         return typeof data === 'string'
             ? JSON.parse(data)
             : JSON.parse(data.toString('utf8'));
